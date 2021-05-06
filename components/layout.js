9e3301ac861df7f586d5a2e55ee13aed24fc25d6
Gabriel Ilídio Souza De Menezes <57880411+gilidio8@users.noreply.github.com>
2021-05-06 16:12:10 -0300

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -30,7 +30,7 @@ export default function Layout({ children, home }) {
           <>
             <Image
               priority
-              src="/public/images/profile.jpg"
+              src="/images/profile.jpg"
               className={utilStyles.borderCircle}
               height={144}
               width={144}
@@ -44,7 +44,7 @@ export default function Layout({ children, home }) {
               <a>
                 <Image
                   priority
-                  src="/public/images/profile.jpg"
+                  src="/images/profile.jpg"
                   className={utilStyles.borderCircle}
                   height={108}
                   width={108}
